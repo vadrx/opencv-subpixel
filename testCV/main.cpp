a41d3062c242ade74aa2ab07a52aa8261678e762
Anton Volkov <vadrx@yandex.ru>
2018-04-12 10:04:03 +0300

    Initial Commit

---
+++ testCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testCV
+//
+//  Created by Anton Volkov on 12/04/2018.
+//  Copyright © 2018 Anton Volkov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
